6a27dce72a34f50ea48414cb7486d4d58a94f7f5
Sergi Canet <s.canet9@outlook.com>
2020-11-19 01:14:27 +0100

    add untracked files to git

---
+++ CosmosDBRestApi/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using CosmosDBRestApi.DataLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CosmosDBRestApi.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private DbContext _context { get; set; }
+        private DataLayer.Infrastructure.Interfaces.IUnitOfWork _uow;
+
+        public UserController(DbContext context, DataLayer.Infrastructure.Interfaces.IUnitOfWork uow)
+        {
+            _context = context;
+            _uow = uow;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUserProva([FromBody] User user)
+        {
+            var entityEntry = await _uow.GetRepository<User>().CreateAsync(user);
+            await _uow.SaveChangesAsync();
+            return Ok(entityEntry);
+        }
+    }
+}
